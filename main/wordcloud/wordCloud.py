fc2cdab39103d3a3c2322aad7ee2c6f317994d66
lsa0828 <lsa0828@duksung.ac.kr>
2024-08-26 19:48:18 +0900

    profile 추가

---
+++ main/wordcloud/wordCloud.py
@@ -0,0 +1,34 @@
+import sys
+import re
+from konlpy.tag import Okt
+from wordcloud import WordCloud
+from collections import Counter
+
+def get_wordcloud(diarys, file_name):
+    # 분석할 데이터 추출
+    text = re.sub(r'[^\w\n]', ' ', diarys)
+    stopwords = [
+            '오늘', '아주', '조금', '매우', '다음', '그때', '너무', '같이', '정말', '모두', '하루', '처음', '시간', '기분', '우리'
+        ]
+
+    # 품사 태깅 : 명사추출
+    okt = Okt()
+    nouns = okt.nouns(text)
+    nouns = [noun for noun in nouns if noun not in stopwords]
+    
+    count = Counter(nouns)
+    
+    wordCount = dict()
+    for tag, counts in count.most_common(30):
+        if(len(str(tag)) > 1):
+            wordCount[str(tag)] = counts
+    
+    fontPath = 'C:/windows/fonts/hmkmmag.ttf'
+    
+    wc = WordCloud(fontPath, background_color='white', width=800, height=600, max_words=25)
+    wordcloud = wc.generate_from_frequencies(wordCount)
+    
+    wordcloud.to_file('./images/' + file_name)
+
+#if __name__ == '__main__':
+#   get_wordcloud(diarys, file_name)
